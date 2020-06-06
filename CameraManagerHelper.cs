deb5770d2cb950326c8522ad202a4a7d507d4442
Tahvohck <Tahvohck@gmail.com>
2020-06-06 14:53:55 -0400

    Add helper for CameraManager => get skybox

---
+++ CameraManagerHelper.cs
@@ -0,0 +1,23 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace Tahvohck_Mods
+{
+    public static class CameraManagerHelper
+    {
+        private static readonly BindingFlags InstanceFlags =
+            BindingFlags.Instance |
+            BindingFlags.NonPublic | BindingFlags.Public;
+
+        public static GameObject GetSkyboxCamera(this CameraManager manager)
+        {
+            FieldInfo skydomeField = typeof(CameraManager).GetField("mSkydomeCamera", InstanceFlags);
+            return (GameObject)skydomeField.GetValue(manager);
+        }
+    }
+}
