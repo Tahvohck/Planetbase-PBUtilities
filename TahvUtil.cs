a1c5db51e10f87a6775592440cef25eb3e791716
Tahvohck <Tahvohck@gmail.com>
2020-06-14 17:55:43 -0400

    Add buffered logger

--- TahvUtil.cs
+++ TahvUtil.cs
@@ -1,5 +1,40 @@
 ﻿using System;
+using UnityModManagerNet;
 
 namespace Tahvohck_Mods
 {
+    using ModData = UnityModManager.ModEntry;
+    public class BufferedLogger
+    {
+        private ModData.ModLogger _Logger;
+        private string _Buffer = "";
+
+        public BufferedLogger(ModData e)
+        {
+            _Logger = e.Logger;
+        }
+
+        public void Buffer(object input)
+        {
+            if (_Logger is null) return;
+            if (_Buffer is "") {
+                _Buffer = $"{input}";
+            } else {
+                _Buffer += $"\n{input}";
+            }
+        }
+
+        public void Flush()
+        {
+            if (_Buffer is "" || _Logger is null) return;
+            _Logger.Log(_Buffer);
+            _Buffer = "";
+        }
+
+        public void Write(object input)
+        {
+            Buffer(input);
+            Flush();
+        }
+    }
 }
