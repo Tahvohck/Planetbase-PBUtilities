300f697f50a9ceee4bd857aca5f4226f0e38346b
Tahvohck <Tahvohck@gmail.com>
2020-06-15 03:36:35 -0400

    Add way to get active module and active module size.

--- GameStateHelper.cs
+++ GameStateHelper.cs
@@ -7,8 +7,15 @@ using System.Reflection;
 
 namespace Tahvohck_Mods
 {
+    using Module = Planetbase.Module;
+
     public static class GameStateHelper
     {
+        public static BindingFlags instanceFlags =
+            BindingFlags.Instance |
+            BindingFlags.NonPublic |
+            BindingFlags.Public;
+
         /// <summary>
         /// Created directly from GameStateGame.Mode. Direct casts are possible.
         /// </summary>
@@ -38,10 +45,42 @@ namespace Tahvohck_Mods
             // This should work on both the Patched and non-patched version of the game,
             // since we ask reflection for public and non public fields.
             return (Mode)game.GetType()
-                .GetField("mMode", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .GetField("mMode", instanceFlags)
                 .GetValue(game);
 
 
+        }
+
+        /// <summary>
+        /// Get the active module, or null if game is null.
+        /// </summary>
+        /// <param name="game"></param>
+        /// <returns></returns>
+        public static Module GetActiveModule(this GameStateGame game)
+        {
+            if (game is null) {
+                return null;
+            }
+
+            return (Module)game.GetType()
+                .GetField("mActiveModule", instanceFlags)
+                .GetValue(game);
+        }
+
+        /// <summary>
+        /// Get mCurrentModuleSize, or zero if game is null.
+        /// </summary>
+        /// <param name="game"></param>
+        /// <returns></returns>
+        public static int GetActiveModuleSizeIndex(this GameStateGame game)
+        {
+            if (game is null) {
+                return 0;
+            }
+
+            return (int)game.GetType()
+                .GetField("mCurrentModuleSize", instanceFlags)
+                .GetValue(game);
         }
     }
 }
